0758acbc7e4645bdef891c32231a9d63ddec85bc
Krisjanis Veinbahs <krisjanis.veinbahs@gmail.com>
2020-09-22 22:14:23 +0300

    Implement homework using pattern matching and wikipedia

--- src/main/scala/com.aahsk.homeworks/basics/Main.scala
+++ src/main/scala/com.aahsk.homeworks/basics/Main.scala
@@ -1,6 +1,12 @@
 package com.aahsk.homeworks.basics
 
 object Basics {
-  def lcm(a: Int, b: Int): Int = ???
-  def gcd(a: Int, b: Int): Int = ???
+  // Algorithm sourced from https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+  def lcm(a: Int, b: Int): Int = Math.abs(a * b) / gcd(a, b)
+
+  // Algorithm sourced from https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid.27s_algorithm
+  def gcd(a: Int, b: Int): Int = b match {
+    case 0 => Math.abs(a)
+    case _ => gcd(b, a % b)
+  }
 }
