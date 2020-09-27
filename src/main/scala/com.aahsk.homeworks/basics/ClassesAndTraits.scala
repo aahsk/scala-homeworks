6d9422a8b55cd1718fa3ebaa9dff505776a31440
Krisjanis Veinbahs <krisjanis.veinbahs@gmail.com>
2020-09-27 21:07:59 +0300

    Another WIP on homework #2

--- src/main/scala/com.aahsk.homeworks/basics/ClassesAndTraits.scala
+++ src/main/scala/com.aahsk.homeworks/basics/ClassesAndTraits.scala
@@ -22,16 +22,16 @@ object Traits {
   sealed trait Shape[D <: Dimensions[D]] extends Located[D] with Bounded[D] with Moveable[D]
 
   sealed trait Located[D <: Dimensions[D]] {
-    val position: D
+    val position: Dimensions[D]
   }
 
   sealed trait Bounded[D <: Dimensions[D]] {
-    def min: D
-    def max: D
+    def min: Dimensions[D]
+    def max: Dimensions[D]
   }
 
   sealed trait Moveable[D <: Dimensions[D]] {
-    def move(delta: D): Shape[D]
+    def move(delta: Dimensions[D]): Shape[D]
   }
 }
 
@@ -39,114 +39,89 @@ object Objects {
   import Traits._;
   import Manipulations._;
 
-  final case class D1(x: Double)                       extends Dimensions[D1]
-  final case class D2(x: Double, y: Double)            extends Dimensions[D2]
-  final case class D3(x: Double, y: Double, z: Double) extends Dimensions[D3]
+  final case class D1(x: Double = 0)                               extends Dimensions[D1]
+  final case class D2(x: Double = 0, y: Double = 0)                extends Dimensions[D2]
+  final case class D3(x: Double = 0, y: Double = 0, z: Double = 0) extends Dimensions[D3]
 
-  final case class Point[D <: Dimensions[D]](position: D) extends Shape[D] {
-    def min                      = position
-    def max                      = position
-    def move(delta: D): Point[D] = Point(addDimensions(position, delta))
+  final case class Point[D <: Dimensions[D]](position: Dimensions[D]) extends Shape[D] {
+    def min: Dimensions[D]                   = position
+    def max: Dimensions[D]                   = position
+    def move(delta: Dimensions[D]): Point[D] = Point(addDimensions(position, delta))
   }
 
   object OriginD1 extends Located[D1] {
-    val position = D1(0)
+    val position: Dimensions[D1] = D1()
   }
 
   object OriginD2 extends Located[D2] {
-    val position = D2(0, 0)
+    val position: Dimensions[D2] = D2()
   }
 
   object OriginD3 extends Located[D3] {
-    val position = D3(0, 0, 0)
+    val position: Dimensions[D3] = D3()
   }
 
-//  final case class Circle(centerX: Double, centerY: Double, radius: Double) extends Shape[Circle] {
-//    def x: Double                            = centerX
-//    def y: Double                            = centerY
-//    def minX: Double                         = centerX - radius
-//    def maxX: Double                         = centerX + radius
-//    def minY: Double                         = centerY - radius
-//    def maxY: Double                         = centerY + radius
-//    def move(dx: Double, dy: Double): Circle = Circle(x + dx, y + dy, radius)
-//  }
+  final case class Circle(center: Dimensions[D2], radius: Double) extends Shape[D2] {
+    val position: Dimensions[D2]            = center
+    def min: Dimensions[D2]                 = position subtract D2(radius, radius)
+    def max: Dimensions[D2]                 = center add D2(radius, radius)
+    def move(delta: Dimensions[D2]): Circle = Circle(center add delta, radius)
+  }
 
-//  final case class Rectangle(
-//      var centerX: Double,
-//      var centerY: Double,
-//      var width: Double,
-//      var height: Double
-//  ) extends Shape[Rectangle] {
-//    def x: Double                               = centerX
-//    def y: Double                               = centerY
-//    def minX: Double                            = centerX - width / 2
-//    def maxX: Double                            = centerX + width / 2
-//    def minY: Double                            = centerY - height / 2
-//    def maxY: Double                            = centerY + height / 2
-//    def move(dx: Double, dy: Double): Rectangle = Rectangle(x + dx, y + dy, width, height)
-//  }
-//
-//  final case class Rectangle(
-//      var centerX: Double,
-//      var centerY: Double,
-//      var width: Double,
-//      var height: Double
-//  ) extends Shape[Rectangle] {
-//    def x: Double                               = centerX
-//    def y: Double                               = centerY
-//    def minX: Double                            = centerX - width / 2
-//    def maxX: Double                            = centerX + width / 2
-//    def minY: Double                            = centerY - height / 2
-//    def maxY: Double                            = centerY + height / 2
-//    def move(dx: Double, dy: Double): Rectangle = Rectangle(x + dx, y + dy, width, height)
-//  }
+  final case class Rectangle(position: Dimensions[D2], bound: Dimensions[D2]) extends Shape[D2] {
+    def min: Dimensions[D2]                    = position subtract bound
+    def max: Dimensions[D2]                    = position add bound
+    def move(delta: Dimensions[D2]): Rectangle = Rectangle(position add delta, bound)
+  }
+
+  final case class Cuboid(position: Dimensions[D3], bound: Dimensions[D3]) extends Shape[D3] {
+    def min: Dimensions[D3]                 = position subtract bound
+    def max: Dimensions[D3]                 = position add bound
+    def move(delta: Dimensions[D3]): Cuboid = Cuboid(position add delta, bound)
+  }
+
+  final case class Cube(position: Dimensions[D3], size: Double) extends Shape[D3] {
+    def min: Dimensions[D3]               = position subtract D3(size, size, size)
+    def max: Dimensions[D3]               = position add D3(size, size, size)
+    def move(delta: Dimensions[D3]): Cube = Cube(position add delta, size)
+  }
 }
 
 object Manipulations {
   import Traits._;
   import Objects._;
 
-//  implicit case class DimensionMonuple(monuple: Tuple1[Double]) {
-//    implicit def toDimensions(): Dimensions1 = Helpers.dimensions1(monuple._1)
-//  }
-//
-//  implicit case class DimensionTuple(monuple: Tuple1[Double]) {
-//    implicit def toDimensions(): Dimensions1 = Helpers.dimensions1(monuple._1)
-//  }
-//
-//  implicit case class DimensionTriple(monuple: Tuple1[Double]) {
-//    implicit def toDimensions(): Dimensions1 = Helpers.dimensions1(monuple._1)
-//  }
-
-//  final case class Meta[D <: Dimensions[D]]() {
-//
-//  }
-
-  def addDimensions[D <: Dimensions[D]](d1: Dimensions[D], d2: Dimensions[D]): D =
+  def addDimensions[D <: Dimensions[D]](d1: Dimensions[D], d2: Dimensions[D]): Dimensions[D] =
     (d1, d2) match {
-      case (D1(x1), D1(x2))         => D1(x1 + x2).asInstanceOf[D]
-      case (D2(x1, y1), D2(x2, y2)) => D2(x1 + x2, y1 + y2).asInstanceOf[D]
+      case (D1(x1), D1(x2))         => D1(x1 + x2).asInstanceOf[Dimensions[D]]
+      case (D2(x1, y1), D2(x2, y2)) => D2(x1 + x2, y1 + y2).asInstanceOf[Dimensions[D]]
       case (D3(x1, y1, z1), D3(x2, y2, z2)) =>
-        D3(x1 + x2, y1 + y2, z1 + z2).asInstanceOf[D]
+        D3(x1 + x2, y1 + y2, z1 + z2).asInstanceOf[Dimensions[D]]
+    }
+
+  def negateDimensions[D <: Dimensions[D]](d: Dimensions[D]): Dimensions[D] =
+    d match {
+      case (D1(x))       => D1(-x).asInstanceOf[Dimensions[D]]
+      case (D2(x, y))    => D2(-x, -y).asInstanceOf[Dimensions[D]]
+      case (D3(x, y, z)) => D3(-x, -y, -z).asInstanceOf[Dimensions[D]]
     }
 
   implicit class DimensionOps[D <: Dimensions[D]](value: Dimensions[D]) {
+    def negate(): Dimensions[D]                   = negateDimensions(value)
     def add(value2: Dimensions[D]): Dimensions[D] = addDimensions(value, value2)
+    def subtract(value2: Dimensions[D]): Dimensions[D] =
+      addDimensions(value, negateDimensions(value2))
   }
 
-//  def minimumBoundingRectangle(objects: Set[Bounded]): Bounded = {
-//    new Bounded {
-//      implicit private val doubleOrdering: Ordering[Double] = Ordering.Double.IeeeOrdering
-//
-//      // if needed, fix the code to be correct
-//      override def minX: Double = objects.map(_.minX).min
-//      override def maxX: Double = objects.map(_.maxX).max
-//      override def minY: Double = objects.map(_.minY).min
-//      override def maxY: Double = objects.map(_.maxY).max
-//    }
+//  def minimumBound[D <: Dimensions[D]](objects: Set[Bounded[D]]): Bounded[D] = {
+//    objects
+////    new Bounded[Dimensions[D]] {
+////      implicit private val doubleOrdering: Ordering[Double] = Ordering.Double.IeeeOrdering
+////
+////    }
 //  }
-//
-//  implicit class BoundedOps[A <: Set[Bounded]](var objects: A) {
-//    def minimumBoundingRectangle(): Bounded = Ops.minimumBoundingRectangle(objects)
+
+//  implicit class BoundedOps[A, D <: Set[Bounded[D]]](var objects: A) {
+//    def minimumBoundingRectangle(): Bounded[D] = Manipulations.minimumBound[D](objects)
 //  }
 }
